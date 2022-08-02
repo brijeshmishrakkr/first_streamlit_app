4e7b191afc3cad5c2a83fff6a6936f70c3298b74
brijeshmishrakkr <110344553+brijeshmishrakkr@users.noreply.github.com>
2022-08-02 11:17:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,10 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
+# Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", # I am getting error in these 2 lines, trying copy paste 
 list(my_fruit_list.index))
 
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))               
+           
